8e2e715c8c3035d6af4be5eef69f64cef2cf15b3
jasonthorne <jasonthorne@live.ie>
2021-09-21 16:27:57 +0100

    UsingVariables

---
+++ R_Programming_A-Z/Section2/12_UsingVariables.R
@@ -0,0 +1,23 @@
+A <- 10
+B <- 5
+C <- A+B
+# print value of C:
+C 
+
+#--------
+var1 <- 2.5
+var2 <- 4
+
+result <- var1/var2
+result
+
+# square root:
+answer <- sqrt(var2)
+answer
+
+# ------------
+# concatenating 2 character vars:
+greeting <- "Hello,"
+name <- "Jason!"
+message <- paste(greeting, name) # use PASTE function to combine character vars! 
+message
