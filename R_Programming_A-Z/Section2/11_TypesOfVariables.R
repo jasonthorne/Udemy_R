542869fd810f3d53cc25aa67a8300cdbab0b8fa1
jasonthorne <jasonthorne@live.ie>
2021-09-21 16:08:08 +0100

    TypesOfVariables

---
+++ R_Programming_A-Z/Section2/11_TypesOfVariables.R
@@ -0,0 +1,20 @@
+# integer:
+x <- 2L # L marks it as an int (otherwise it's a double by default)
+typeof(x) # check type of x
+
+# double:
+y <- 2.5
+typeof(y) # check type of y
+
+#complex:
+z <- 3 + 2i
+typeof(z) # check type of z
+
+#character:
+a <- "h"
+typeof(a) # check type of a
+
+#logical (bool):
+q <- T # 'T' works instead of 'TRUE' (must be in caps though :P)
+typeof(q) # check type of q
+
