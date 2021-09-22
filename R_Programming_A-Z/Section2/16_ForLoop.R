b69faf051cb9dfccabe9755ec3f11032390516ac
jasonthorne <jasonthorne@live.ie>
2021-09-22 21:48:50 +0100

    ForLoop

---
+++ R_Programming_A-Z/Section2/16_ForLoop.R
@@ -0,0 +1,10 @@
+
+# for loops have to use vectors to iterate. (1:5 creates a vector(list) from 1-5)
+
+for(i in 1:5){
+  print(i)
+}
+  
+for(i in 6:10){ # also prints 5 times (6-10)
+  print(i)
+}
