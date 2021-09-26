f98fbbc3f851dfb1305019598dbde8462170ee2e
jasonthorne <jasonthorne@live.ie>
2021-09-26 22:20:27 +0100

    If

--- R_Programming_A-Z/Section2/17_IfStatement.R
+++ R_Programming_A-Z/Section2/17_IfStatement.R
@@ -1,5 +1,12 @@
 
-# create random number:
-rnorm(1)
+rm(answer) # remove answer var from global values
 
+x <- rnorm(1) # create random number inside a variable
 
+if(x > 1){
+ answer <- "x is greater than 1"
+} else if (x >= -1) {
+   answer <- "x is between -1 & 1"
+}else{
+   answer <- "x is less than -1"
+}
