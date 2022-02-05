185a31f3b916ca82afe06c67fa404ddb6353f0f9
jasonthorne <jasonthorne@live.ie>
2022-02-05 23:33:04 +0000

    Building Matrix 2

---
+++ R_Programming_A-Z/Section4/33_BuildingYourFirstMatrix.R
@@ -0,0 +1,20 @@
+
+# ++++++++++ run BasketballData.R first :P 
+
+Salary # show salary matrix
+Games # show games matrix
+MinutesPlayed # etc.. 
+
+#---------------------------------------------------------------------
+
+# rbind() 'row bind'
+# binds each vector in a list of vectors to their associated row in a matrix
+# e.g: first vector is added to row one in matrix, 2nd to row 2 etc
+
+
+# cbind() ''col bind'
+# binds each element in a vector to their associated column in a matrix. 
+
+
+
+
