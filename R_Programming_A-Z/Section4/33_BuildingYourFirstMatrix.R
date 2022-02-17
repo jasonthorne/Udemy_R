fb40e314d35c9f853a0834a0f2c9fb1c818042d2
jasonthorne <jasonthorne@live.ie>
2022-02-17 23:42:24 +0000

    Building Matrix

--- R_Programming_A-Z/Section4/33_BuildingYourFirstMatrix.R
+++ R_Programming_A-Z/Section4/33_BuildingYourFirstMatrix.R
@@ -16,4 +16,29 @@ MinutesPlayed # etc..
 # binds each element in a vector to their associated column in a matrix. 
 
 
+#--------------
+
+#matrix() 
+?matrix
+
+myData <- 1:20 #create vector of 1-2
+myData
+
+# create matrix from vector:
+myMatrix <- matrix(myData, 4, 5) # data, no of rows, no of cols 
+# ++++++++NOTE: row x col above MUST equal to length of vector (otherwise null elements)
+
+myMatrix
+
+
+
+
+
+
+
+
+
+
+
+
 
