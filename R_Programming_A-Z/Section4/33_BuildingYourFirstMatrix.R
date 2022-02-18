402186e3bbc1271e57e84952464fdc853333f021
jasonthorne <jasonthorne@live.ie>
2022-02-18 00:01:46 +0000

    Building Matrix

--- R_Programming_A-Z/Section4/33_BuildingYourFirstMatrix.R
+++ R_Programming_A-Z/Section4/33_BuildingYourFirstMatrix.R
@@ -18,27 +18,45 @@ MinutesPlayed # etc..
 
 #--------------
 
-#matrix() 
+#matrix() - creates a matrix
 ?matrix
 
 myData <- 1:20 #create vector of 1-2
 myData
 
 # create matrix from vector:
-myMatrix <- matrix(myData, 4, 5) # data, no of rows, no of cols 
+myMatrix1 <- matrix(myData, 4, 5) # data, no of rows, no of cols 
 # ++++++++NOTE: row x col above MUST equal to length of vector (otherwise null elements)
 
-myMatrix
+myMatrix1
+myMatrix1[2,3] # print 10
 
 
+myMatrix2 <- matrix(myData, 4, 5, byrow=TRUE) # populates the matrix BY ROW instead of col
+myMatrix2
+myMatrix2[2,5] # print 10
 
+#------------
 
+#rbind() - bind by row
 
+#make 3 vectors:
+r1 <- c("I", "am", "happy")
+r2 <- c("I", "like", "trains")
+r3 <- c(1, 2, 3)
 
+myMatrix3 <- rbind(r1,r2,r3) # combine vectors into a matrix BY ROW
+myMatrix3 # ++++++++++ Note that ints get converted to chars as you cant mix types
 
+#-------------
 
+#cbind() - bind by col
 
+#make 3 vectors:
+c1 <- c("I", "am", "happy")
+c2 <- c("I", "like", "trains")
+c3 <- c(1, 2, 3)
 
-
-
+myMatrix4 <- cbind(c1,c2,c3) # combine vectors into a matrix BY COL
+myMatrix4 # ++++++++++ Note that ints get converted to chars as you cant mix types
 
